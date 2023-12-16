709bd1affb903be9596030489b3c128e2e1ad685
RoniAbravaya <150835357+RoniAbravaya@users.noreply.github.com>
2023-12-16 13:20:59 +0200

    update

---
+++ week3/dailychallange3/script.js
@@ -0,0 +1,30 @@
+// Instructions
+// Write a JavaScript program that recreates the pattern below.
+// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out this tutorial of nested loops).
+// Do this Daily Challenge by yourself, without looking at the answers on the internet.
+
+
+const numberOfRows = 6;
+
+for (let i = 1; i <= numberOfRows; i++) {
+  let row = '';
+  for (let j = 1; j <= i; j++) {
+    row += '* ';
+  }
+  console.log(row);
+}
+
+
+//
+
+const numberOfRowss = 6;
+
+for (let i = 1; i <= numberOfRowss; i++) {
+  let row = '';
+  for (let j = 1; j <= i; j++) {
+    row += '* ';
+  }
+  console.log(row);
+}
+
+
