118309dbafbf58b36e741b61da4689ad54fb5095
RoniAbravaya <150835357+RoniAbravaya@users.noreply.github.com>
2024-01-26 18:03:13 +0200

    update

---
+++ week9/day4/ExercisesXP/script.js
@@ -0,0 +1,90 @@
+// 🌟 Exercise 1 : HTML Form
+// Instructions
+// Hint : Read about sending form data using the HTTP protocol
+
+// form
+
+// Create a form like the one above. The form should contain three inputs:
+// a small text input asking for a name,
+// a larger textarea input to write a message,
+// a submit input (“Send”)
+
+// When you click the Send button, the form will be submitted with a GET method. (you can use the same HTML file for the action url)
+// Where will the sent data appear?
+
+
+
+// file:///C:/Users/erolr/Desktop/DI_Bootcamp_Stage1/DI%20bootcamp/week9/day4/ExercisesXP/index.html?name=test&message=test#
+
+
+// 🌟 Exercise 2 : HTML Form #2
+// Instructions
+// Use the same form structure as Exercise 1.
+// When you click the Send button, the form will be submitted with a POST method.(you can use the same HTML file for the action url)
+// Where will the sent data appear? Hint : Look at the Network Tab
+
+// In the Network tab, you will see a request being made with the method POST, and you can inspect the request payload to view the form data being sent in the request body. The data won't be visible in the URL as it would be with the GET method.
+
+
+
+// 🌟 Exercise 3 : JSON Mario
+// Instructions
+// Using this code:
+
+// const marioGame = {
+//   detail : "An amazing game!",
+//   characters : {
+//       mario : {
+//         description:"Small and jumpy. Likes princesses.",
+//         height: 10,
+//         weight: 3,
+//         speed: 12,
+//       },
+//       bowser : {
+//         description: "Big and green, Hates princesses.",
+//         height: 16,
+//         weight: 6,
+//         speed: 4,
+//       },
+//       princessPeach : {
+//         description: "Beautiful princess.",
+//         height: 12,
+//         weight: 2,
+//         speed: 2,
+//       }
+//   },
+// }
+// Convert this JS object into a JSON object. What happens to the nested objects ?
+// Convert and pretty print this JS object into a JSON object. Hint : Check out the JSON lesson on the platform.
+// Use your web inspector to add a breakpoint. Check the values of the JSON object in the debugger.
+
+const marioGame = {
+    detail: "An amazing game!",
+    characters: {
+      mario: {
+        description: "Small and jumpy. Likes princesses.",
+        height: 10,
+        weight: 3,
+        speed: 12,
+      },
+      bowser: {
+        description: "Big and green, Hates princesses.",
+        height: 16,
+        weight: 6,
+        speed: 4,
+      },
+      princessPeach: {
+        description: "Beautiful princess.",
+        height: 12,
+        weight: 2,
+        speed: 2,
+      },
+    },
+  };
+  
+  // Convert to JSON and pretty-print
+  const jsonMarioGame = JSON.stringify(marioGame, null, 2);
+  
+  // Log the JSON object
+  console.log(jsonMarioGame);
+  
