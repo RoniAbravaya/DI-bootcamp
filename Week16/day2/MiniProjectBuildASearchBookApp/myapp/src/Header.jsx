cfa358cf282dd5c9da4da90d1ace5a74adc2a08a
RoniAbravaya <150835357+RoniAbravaya@users.noreply.github.com>
2024-03-16 13:39:44 +0200

    update

---
+++ Week16/day2/MiniProjectBuildASearchBookApp/myapp/src/Header.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+  
+    return (
+      <header>
+        <i className="fas fa-book fa-2x"></i>
+        <h1>Book Cards</h1>
+      </header>
+      
+    );
+}
+
+export default Header;
+
+
+
+
+
+// WEBPACK FOOTER //
+// ./src/Header.js
