25c9c5ec0e1ceeadbb82f69b0d98e1d260dec93b
RoniAbravaya <150835357+RoniAbravaya@users.noreply.github.com>
2024-05-13 17:38:35 +0300

    update

---
+++ week21/day2/DailyChallengePagination/DailyChallenge.py
@@ -0,0 +1,32 @@
+class Pagination:
+    def __init__(self, items=None, pageSize=10):
+        self.items = items or []
+        self.pageSize = int(pageSize)
+        self.totalPages = (len(self.items) + self.pageSize - 1) // self.pageSize
+        self.currentPage = 1
+
+    def getVisibleItems(self):
+        start = (self.currentPage - 1) * self.pageSize
+        end = start + self.pageSize
+        return self.items[start:end]
+
+    def prevPage(self):
+        self.currentPage = max(1, self.currentPage - 1)
+        return self
+
+    def nextPage(self):
+        self.currentPage = min(self.totalPages, self.currentPage + 1)
+        return self
+
+    def firstPage(self):
+        self.currentPage = 1
+        return self
+
+    def lastPage(self):
+        self.currentPage = self.totalPages
+        return self
+
+    def goToPage(self, pageNum):
+        pageNum = int(pageNum)
+        self.currentPage = max(1, min(self.totalPages, pageNum))
+        return self
